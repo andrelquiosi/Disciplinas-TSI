59679d77152a1ce3f1b0a6a8c1e9d726a8efbde0
andrelquiosi <andre.quiosi@Outlook.com>
2023-04-23 11:19:14 -0300

    new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/.gitignore" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/.mvn/wrapper/maven-wrapper.jar" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/.mvn/wrapper/maven-wrapper.properties" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/pom.xml" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplication.java" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/src/main/resources/application.properties" 	new file:   "5_semestre/T\303\223PICOS AVAN\303\207ADOS EM TECNOLOGIA DA INFORMA\303\207\303\203O/Trabalho_1/projeto_delegacia/src/test/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplicationTests.java"

---
+++ 5_semestre/TÓPICOS AVANÇADOS EM TECNOLOGIA DA INFORMAÇÃO/Trabalho_1/projeto_delegacia/src/main/java/br/edu/utfpr/td/tsi/projeto_delegacia/ProjetoDelegaciaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.projeto_delegacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDelegaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDelegaciaApplication.class, args);
+	}
+
+}
